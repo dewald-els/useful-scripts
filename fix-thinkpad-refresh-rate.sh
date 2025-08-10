da9f734d6559b9c39ed1a742660a674f884084d3
Dewald Els <hello@dewaldels.com>
2025-08-10 09:37:04 +0200

    Update fix-thinkpad-refresh-rate.sh

--- fix-thinkpad-refresh-rate.sh
+++ fix-thinkpad-refresh-rate.sh
@@ -9,3 +9,16 @@ sudo grub2-mkconfig -o /boot/grub2/grub.cfg
 
 # restart system
 sudo reboot
+
+## When using systemd
+
+sudo nano /boot/loader/entries/<some-file-name>.conf
+
+# Edit the file with the options
+
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /initramfs-linux.img
+options root=UUID=your-root-uuid rw amdgpu.dcdebugmask=0x8000 # <-- Add debug mask here 
+
+# Reboot.
